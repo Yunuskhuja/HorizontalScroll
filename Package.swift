c158cc931f4bb0400f741cc7757362d75bb3d5d6
Yunusxoja Tuygunxojayev <y.tuygunxojayev@uzum.com>
2023-11-13 08:31:16 +0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HorizontalScroll",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "HorizontalScroll",
+            targets: ["HorizontalScroll"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "HorizontalScroll"),
+        .testTarget(
+            name: "HorizontalScrollTests",
+            dependencies: ["HorizontalScroll"]),
+    ]
+)
